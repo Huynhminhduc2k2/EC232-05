84781b677f8e7ebbce5db5d939136b42da4f4a6c
HenryHTN <105685674+HenryHTN@users.noreply.github.com>
2023-09-05 20:39:36 +0700

    Update server.js

--- Source/DB/Server/server.js
+++ Source/DB/Server/server.js
@@ -23,6 +23,30 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+const ThanhVien = mongoose.model('ThanhVien', new mongoose.Schema({
+  HoTen: String,
+  GioiTinh: String,
+  NamSinh: String,
+  SDT: String,
+  Email: String,
+}));
+
+app.post("/post", async(req, res)=>{
+  console.log("inside post function");
+
+  const data = new ThanhVien({
+    HoTen:req.body.HoTen,
+    GioiTinh:req.body.GioiTinh,
+    NamSinh:req.body.NamSinh,
+    SDT:req.body.SDT,
+    Email:req.body.Email
+  });
+
+  const val = await data.save();
+  res.json(val);
+
+})
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
