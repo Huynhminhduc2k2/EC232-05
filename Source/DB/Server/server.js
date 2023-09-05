38021a0d32cc25c4af17b2a7f5065646c35d5b25
Huân <truonghuan0709@gmail.com>
2023-09-05 11:09:50 +0700

    Server
    - Connected server to MongoDB

---
+++ Source/DB/Server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const uri = 'mongodb+srv://admin:0335597676Huan@demo1.gky5n1m.mongodb.net/?retryWrites=true&w=majority';
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+}
+connect();  // Thêm dấu () để gọi hàm connect()
+
+const app = express();
+
+app.get("/", (req, res) => {
+  // Xử lý tuyến đường GET "/"
+  res.send("Hello, World!");
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
