590a89c8f949b6330abe513afdf669d935529aec
Huân <truonghuan0709@gmail.com>
2023-09-05 21:26:46 +0700

    fix bug
    sửa lỗi ở file server.js cho Huy

--- Source/DB/Server/server.js
+++ Source/DB/Server/server.js
@@ -1,22 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import bodyParser from "body-parser"; // Thêm body-parser để xử lý dữ liệu gửi lên
 
 dotenv.config();
 
-const uri = 'mongodb+srv://admin:0335597676Huan@demo1.gky5n1m.mongodb.net/?retryWrites=true&w=majority';
+const uri = process.env.MONGODB_URI; // Thay thế URI kết nối MongoDB bằng biến môi trường
 
 async function connect() {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(error);
   }
 }
-connect();  // gọi hàm connect()
+connect();
 
 const app = express();
+
+app.use(bodyParser.json()); // Sử dụng body-parser để xử lý JSON data
 
 app.get("/", (req, res) => {
   // Xử lý tuyến đường GET "/"
@@ -31,23 +37,27 @@ const ThanhVien = mongoose.model('ThanhVien', new mongoose.Schema({
   Email: String,
 }));
 
-app.post("/post", async(req, res)=>{
+app.post("/post", async (req, res) => {
   console.log("inside post function");
 
   const data = new ThanhVien({
-    HoTen:req.body.HoTen,
-    GioiTinh:req.body.GioiTinh,
-    NamSinh:req.body.NamSinh,
-    SDT:req.body.SDT,
-    Email:req.body.Email
+    HoTen: req.body.HoTen,
+    GioiTinh: req.body.GioiTinh,
+    NamSinh: req.body.NamSinh,
+    SDT: req.body.SDT,
+    Email: req.body.Email,
   });
 
-  const val = await data.save();
-  res.json(val);
+  try {
+    const val = await data.save();
+    res.json(val);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Lỗi khi lưu dữ liệu.' });
+  }
+});
 
-})
-
-const port = process.env.PORT;
+const port = process.env.PORT || 3000; // Sử dụng cổng môi trường hoặc mặc định là 3000
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
