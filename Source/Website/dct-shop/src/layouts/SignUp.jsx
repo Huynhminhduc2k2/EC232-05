ab19d50f7b9c861b5fed05f9402bca2a515aa05f
Huynhminhduc2k2 <hmduc20@clc.fitus.edu.vn>
2023-09-06 23:48:15 +0700

    Axios cho đn, đk

--- Source/Website/dct-shop/src/layouts/SignUp.jsx
+++ Source/Website/dct-shop/src/layouts/SignUp.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-import '../styleSheet/SignUp.css'
-import HeaderOrigin from './HeaderOrigin'
-import Footer from './Footer'
+import React, { useState } from 'react';
+import '../styleSheet/SignUp.css';
+import HeaderOrigin from './HeaderOrigin';
+import Footer from './Footer';
+import axios from '../AxiosConfig';
 
 function SignUp() {
+  const [accountInfo, setAccountInfo] = useState({
+    TenTaiKhoan: '',
+    MatKhau: '',
+    HoTen: '',
+    GioiTinh: '',
+    NamSinh: '',
+    SDT: '',
+    Email: '',
+  });
+
+  const [message, setMessage] = useState('');
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setAccountInfo({
+      ...accountInfo,
+      [name]: value,
+    });
+  };
+
+  const handleRegistration = async () => {
+    try {
+      const response = await axios.post('/register', accountInfo);
+
+      if (response.status === 201) {
+        // Đăng ký thành công, hiển thị thông báo thành công
+        setMessage('Tài khoản đã được đăng ký thành công.');
+      } else {
+        // Xử lý lỗi đăng ký, hiển thị thông báo lỗi
+        setMessage('Đã có lỗi xảy ra khi đăng ký tài khoản.');
+      }
+    } catch (error) {
+      console.error(error);
+      // Xử lý lỗi mạng hoặc lỗi máy chủ
+      setMessage('Đã có lỗi xảy ra khi đăng ký tài khoản.');
+    }
+  };
+
   return (
     <>
       <section className="SignUp">
@@ -16,38 +55,78 @@ function SignUp() {
               <input
                 className="accountName"
                 type="text"
+                name="TenTaiKhoan"
                 placeholder="Tên tài khoản"
+                value={accountInfo.TenTaiKhoan}
+                onChange={handleInputChange}
+              />
+              <hr />
+              <input
+                className="accountPass-SignUp"
+                type="password"
+                name="MatKhau"
+                placeholder="Mật khẩu"
+                value={accountInfo.MatKhau}
+                onChange={handleInputChange}
+              />
+              <hr />
+              <input
+                className="accountName-SignUp"
+                type="text"
+                name="HoTen"
+                placeholder="Họ tên"
+                value={accountInfo.HoTen}
+                onChange={handleInputChange}
+              />
+              <hr />
+              <input
+                className="accountGender"
+                type="text"
+                name="GioiTinh"
+                placeholder="Giới tính"
+                value={accountInfo.GioiTinh}
+                onChange={handleInputChange}
+              />
+              <hr />
+              <input
+                className="accountDoB"
+                type="text"
+                name="NamSinh"
+                placeholder="Năm sinh"
+                value={accountInfo.NamSinh}
+                onChange={handleInputChange}
+              />
+              <hr />
+              <input
+                className="accountPhone"
+                type="text"
+                name="SDT"
+                placeholder="Số điện thoại"
+                value={accountInfo.SDT}
+                onChange={handleInputChange}
               />
               <hr />
               <input
                 className="accountMail"
                 type="text"
+                name="Email"
                 placeholder="Địa chỉ email"
-              />
-              <hr />
-              <input
-                className="accountPass"
-                type="text"
-                placeholder="Mật khẩu"
-              />
-              <hr />
-              <input
-                className="accountPassCheck"
-                type="text"
-                placeholder="Xác nhận mật khẩu"
+                value={accountInfo.Email}
+                onChange={handleInputChange}
               />
               <hr />
             </div>
             <div className="SignUp-btn">
-              <button>ĐĂNG KÝ</button>
+              <button onClick={handleRegistration}>ĐĂNG KÝ</button>
             </div>
+            <div className="Message">{message}</div>
           </div>
         </div>
 
         <Footer />
       </section>
     </>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
